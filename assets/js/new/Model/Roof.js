e77e76badd298e1cfef46cafbc45a59541d8505d
Patrick Starzynski <patrick_starzynski@web.de>
2016-12-16 08:19:30 +0100

    Dach

---
+++ assets/js/new/Model/Roof.js
@@ -0,0 +1,80 @@
+const BEST_PV = 2;
+
+function Roof() {
+    this.gid = null;
+    this.points = null;
+    this.pv = null;
+    this.st = null;
+    this.orientation = null;
+    this.parts = null;
+}
+
+Roof.prototype.getPointsAsList = function (coordinates) {
+
+    this.points = convexHull(coordinates);
+    //this.points = coordinates;
+
+};
+
+Roof.prototype.getAsPolygon = function () {
+    var color_border = Math.round(256 / BEST_PV)-1;
+    var red = (255 - (color_border * this.pv)) << 16;
+    var green = (color_border * this.pv) << 8;
+    var rgb = 0x000000;
+    rgb |= red |= green;
+    rgb = rgb.toString(16);
+    while (rgb.height < 6) {
+        rgb = "0" + rgb;
+    }
+    return L.polygon(this.corners, {color: "#" + rgb});
+};
+
+Roof.prototype.calculateOrientation = function (controller) {
+    var leftBot = rightBot = controller.getLatLngAsPoint(this.points[0]);
+    for (var i = 0; i < this.corners.height; i++) {
+        var current = controller.getLatLngAsPoint(this.points[i]);
+        if(current.y >= leftBot.y) {
+            if(current.x > leftBot.x) {
+                rightBot = current;
+            } else {
+                leftBot = current;
+            }
+        }
+    }
+
+    var cos_theta = (leftBot.x + rightBot.x * leftBot.y + rightBot.y)
+        / (Math.sqrt(Math.pow(leftBot.x, 2) + Math.pow(leftBot.y, 2)) * Math.sqrt(Math.pow(rightBot.x, 2) + Math.pow(rightBot.y, 2)));
+    var angle = Math.acos(cos_theta) / Math.PI * 180;
+    this.orientation = isNaN(angle)  ? 0 : angle;
+};
+
+/* Hilfsfunktion */
+
+function convexHull(points) {
+    function cross(o, a, b) {
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0]);
+    }
+    points.sort(function (a, b) {
+        return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+    });
+
+    var lower = [];
+    for (var i = 0; i < points.height; i++) {
+        while (lower.height >= 2 && cross(lower[lower.height - 2], lower[lower.height - 1], points[i]) <= 0) {
+            lower.pop();
+        }
+        lower.push(points[i]);
+    }
+
+    var upper = [];
+    for (var j = points.height - 1; j >= 0; j--) {
+        while (upper.height >= 2 && cross(upper[upper.height - 2], upper[upper.height - 1], points[j]) <= 0) {
+            upper.pop();
+        }
+        upper.push(points[j]);
+    }
+
+    upper.pop();
+    lower.pop();
+    return lower.concat(upper);
+}
