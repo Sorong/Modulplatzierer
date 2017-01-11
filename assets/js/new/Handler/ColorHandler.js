45d02e089060af6c9fafc4ba138f64110d424774
Andrej <sperling@protonmail.com>
2017-01-11 22:40:28 +0100

    Die Farborientierung 'funktioniert'

--- assets/js/new/Handler/ColorHandler.js
+++ assets/js/new/Handler/ColorHandler.js
@@ -5,9 +5,10 @@ var ColorHandler = (function () {
         this.colorContainer = null;
         this.colors = [];
         this.defaultColor = '#FF0';
-        this.size = 0;
+        this.size = 3;
         this.orientation = 0;
         this.createSvgContainer();
+		this.linearGradient = null;
     }
 
     ColorHandler.prototype.setSize = function (size) {
@@ -32,35 +33,39 @@ var ColorHandler = (function () {
     };
 
     function createGradient(svg, id, stops) {
-        var svgNS = svg.namespaceURI;
-        var grad = document.createElementNS(svgNS, 'linearGradient');
+		if(this.linearGradient == null){
+			var svgNS = svg.namespaceURI;
+			this.linearGradient = document.createElementNS(svgNS, 'linearGradient');
 
-        var degrees = 135;
-        var angleInRadians = degrees * Math.PI / 180;
-        var length = 100;
+			this.linearGradient.setAttribute('id', id);
+			this.linearGradient.setAttribute('spreadMethod', 'pad');
+			
+			var angle = 135;
+			var anglePI = (angle) * (Math.PI / 180);
+			var angleCoords = {
+				'x1': Math.round(50 + Math.sin(anglePI) * 50) + '%',
+				'y1': Math.round(50 + Math.cos(anglePI) * 50) + '%',
+				'x2': Math.round(50 + Math.sin(anglePI + Math.PI) * 50) + '%',
+				'y2': Math.round(50 + Math.cos(anglePI + Math.PI) * 50) + '%',
+			}
+		
+			this.linearGradient.setAttribute('x1', angleCoords.x1);
+			this.linearGradient.setAttribute('y1', angleCoords.y1);
+			this.linearGradient.setAttribute('x2', angleCoords.x2);
+			this.linearGradient.setAttribute('y2', angleCoords.y2);
 
-        var endX = Math.cos(angleInRadians) * length;
-        var endY = Math.sin(angleInRadians) * length;
+			for (var i = 0; i < stops.length; i++) {
+				var attrs = stops[i];
+				var stop = document.createElementNS(svgNS, 'stop');
+				for (var attr in attrs) {
+					if (attrs.hasOwnProperty(attr)) stop.setAttribute(attr, attrs[attr]);
+				}
+				this.linearGradient.appendChild(stop);
+			}
 
-        grad.setAttribute('id', id);
-        grad.setAttribute('x1', 0 + '%');
-        grad.setAttribute('y1', 0 + '%');
-        grad.setAttribute('x2', 100 + '%');
-        grad.setAttribute('y2', 100 + '%');
-
-        grad.setAttribute('gradientTransform', 'rotate(45)');
-
-        for (var i = 0; i < stops.length; i++) {
-            var attrs = stops[i];
-            var stop = document.createElementNS(svgNS, 'stop');
-            for (var attr in attrs) {
-                if (attrs.hasOwnProperty(attr)) stop.setAttribute(attr, attrs[attr]);
-            }
-            grad.appendChild(stop);
-        }
-
-        var defs = svg.querySelector('defs') || svg.insertBefore(document.createElementNS(svgNS, 'defs'), svg.firstChild);
-        return defs.appendChild(grad);
+			var defs = svg.querySelector('defs') || svg.insertBefore(document.createElementNS(svgNS, 'defs'), svg.firstChild);
+			return defs.appendChild(this.linearGradient);
+		}
     }
 
     ColorHandler.prototype.createSvgContainer = function () {
@@ -96,7 +101,9 @@ var ColorHandler = (function () {
             colors.push({offset: prozent + '%', 'stop-color': '' + this.getColor(i)})
         }
 
-        createGradient(this.colorContainer, this.name, [
+		createGradient(this.colorContainer, this.name, colors);
+		
+        /*createGradient(this.colorContainer, this.name, [
             {offset: '0%', 'stop-color': '#ff0'},
             {offset: '33.33%', 'stop-color': '#ff0'},
 
@@ -105,7 +112,7 @@ var ColorHandler = (function () {
 
             {offset: '66.66%', 'stop-color': '#0f0'},
             {offset: '100%', 'stop-color': '#0f0'}
-        ]);
+        ]); */
     };
 
     ColorHandler.prototype.getColorId = function () {
